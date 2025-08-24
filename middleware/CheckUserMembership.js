3e68e18b51474e9cfbcf2e4886f09def7e5e7a01
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-08-24 14:03:12 +0530

    completed role base access control

--- middleware/CheckUserMembership.js
+++ middleware/CheckUserMembership.js
@@ -1,5 +1,7 @@
 const { Verify_JWT_Token } = require('../servies/AuthServices');
 const Memberships = require('../models/Memberships.js');
+const RolePermissionMap = require('../models/RolePermissionMap.js');
+const Permission = require('../models/Permission.js');
 
 const membershipCache = {};
 const rolePermissionMapCache = {};
@@ -17,17 +19,33 @@ const VerifyUserMemebership = async (req, res, next) => {
     const decoded = Verify_JWT_Token(token);
     const user_id = decoded.id
     let role_ids = [];
-    if (!membershipCache.user_id) {
+    if (!membershipCache[user_id]) {
         const membershipModel = await Memberships.distinct('role_id', { user_id: user_id }).lean();
         role_ids = membershipModel;
-        membershipCache.user_id = membershipModel;
+        membershipCache[user_id] = membershipModel;
     } else {
-        role_ids = membershipCache.user_id;
+        role_ids = membershipCache[user_id];
     }
 
     const requestedUrl = req.originalUrl;
-    console.log(requestedUrl);
-    next()
+    const permissionObjectId = await Permission.findOne({ name: requestedUrl }).select('_id').lean();
+    let authrizedUser = false;
+    for (const roleId of role_ids) {
+        if (!rolePermissionMapCache[roleId]) {
+            const permissions = await RolePermissionMap.distinct('permission_id', { role_id: roleId }).lean();
+            rolePermissionMapCache[roleId] = permissions.map(p => p.toString());
+        }
+        if (rolePermissionMapCache[roleId].includes(permissionObjectId._id.toString())) {
+            authrizedUser = true;
+            break;
+        }
+    }
+
+    if (authrizedUser == true) {
+        next()
+    } else {
+        return res.redirect(referer || '/');
+    }
 
 }
 
