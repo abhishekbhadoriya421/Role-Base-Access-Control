946e227b2f986d0c07d91f6b96a04b8388a48468
Abhishe Bhadoria <abhishekbhadauria421@gmail.com>
2025-09-05 22:40:27 +0530

    create cache using redis set cache and get cache functions

--- middleware/CheckUserMembership.js
+++ middleware/CheckUserMembership.js
@@ -2,6 +2,7 @@ const { Verify_JWT_Token } = require('../servies/AuthServices');
 const Memberships = require('../models/Memberships.js');
 const RolePermissionMap = require('../models/RolePermissionMap.js');
 const Permission = require('../models/Permission.js');
+const CacheService = require('../servies/CacheService');
 
 const membershipCache = {};
 const rolePermissionMapCache = {};
@@ -29,15 +30,28 @@ const VerifyUserMemebership = async (req, res, next) => {
 
     /**
      * Get user role if saved in cache fetch from cache else make db query
-     */
-    if (!membershipCache[user_id]) {
-        const membershipModel = await Memberships.distinct('role_id', { user_id: user_id }).lean();
-        role_ids = membershipModel;
-        membershipCache[user_id] = membershipModel;
-    } else {
-        role_ids = membershipCache[user_id];
-    }
+     * */
+    // if (!membershipCache[user_id]) {
+    //     const membershipModel = await Memberships.distinct('role_id', { user_id: user_id }).lean();
+    //     role_ids = membershipModel;
+    //     membershipCache[user_id] = membershipModel;
+    // } else {
+    //     role_ids = membershipCache[user_id];
+    // }
 
+    /**
+     * cache data using redis
+     */
+
+    let roleIds = await CacheService.getCache(user_id);
+    console.log("get from redis ", roleIds)
+    if (!roleIds) {
+        console.log("Role Id not found");
+        const modelId = await Memberships.distinct('role_id', { user_id: user_id }).lean();
+        if (!modelId) { console.log("You don't have assigned any role"); return res.redirect(referer || '/'); }
+        roleIds = await CacheService.setCache(user_id, modelId, 3600);
+        console.log("Role Id Has been Set : ", roleIds);
+    }
     /**
      * get permission id from requested url if cached fetch from there else fetch from db
      */
@@ -57,7 +71,7 @@ const VerifyUserMemebership = async (req, res, next) => {
      * check if user is authorized to visit the request page  
      */
     let authorizedUser = false;
-    for (const roleId of role_ids) {
+    for (const roleId of roleIds) {
         if (!rolePermissionMapCache[roleId]) {
             const permissions = await RolePermissionMap.distinct('permission_id', { role_id: roleId }).lean();
             rolePermissionMapCache[roleId] = permissions.map(p => p.toString());
---
+++ servies/CacheService.js
@@ -0,0 +1,31 @@
+const { redisClient } = require('../config/redis');
+
+/**
+ * TTL => Time To Live
+ */
+const setCache = async (key, value, ttl) => {
+    try {
+        await redisClient.setEx(key, ttl, JSON.stringify(value));
+        console.log("Data has been cached: ");
+        return value;
+    } catch (err) {
+        console.log("From Redis: ", err);
+    }
+}
+
+
+const getCache = async (key) => {
+    try {
+        const value = await redisClient.get(key);
+        if (value) {
+            return JSON.paras(value);
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.log("From Redis: ", err);
+    }
+}
+
+
+module.exports = { setCache, getCache }
